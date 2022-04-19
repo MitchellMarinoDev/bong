eb87443174fd327abc45b4910f651e92223606aa
MitchellMarino <marinodev@tuta.io>
2022-04-19 11:19:39 -0500

    remove physics to shift to heron

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,11 @@ type Server = carrier_pigeon::Server<Connection, Response, Disconnect>;
 
 fn main() {
     App::new()
+        .insert_resource(WindowDescriptor {
+            title: "Warloards".into(),
+            mode: bevy::window::WindowMode::Fullscreen,
+            ..Default::default()
+        })
         .add_startup_system_to_stage(StartupStage::PreStartup, setup)
         .add_plugin(ClientPlugin::<Connection, Response, Disconnect>::new())
         .add_plugins(DefaultPlugins)
@@ -28,59 +33,36 @@ fn setup(
     camera.orthographic_projection.right = 1920.0/2.0;
     camera.orthographic_projection.bottom = -1080.0/2.0;
     camera.orthographic_projection.top = 1080.0/2.0;
-
     commands.spawn()
         .insert_bundle(camera);
 
     commands.spawn()
-        .insert_bundle(ColliderBundle {
-            shape: ColliderShapeComponent(ColliderShape::cuboid(100.0, 200.0)),
-            ..Default::default()
-        })
         .insert_bundle(SpriteBundle {
             sprite: Sprite {
                 color: Color::rgb_u8(255, 50, 50),
-                custom_size: Some(Vec2::new(100.0, 100.0)),
+                custom_size: Some(Vec2::new(200.0, 100.0)),
                 ..Default::default()
             },
-            transform: Transform::from_xyz(0.0, 10.0, 0.0),
             ..Default::default()
         })
         .insert(Brick);
 
     // ball
     commands.spawn()
-        .insert_bundle(ColliderBundle {
-            shape: ColliderShapeComponent(ColliderShape::ball(10.0)),
-            ..Default::default()
-        })
-        .insert_bundle(RigidBodyBundle {
-            body_type: RigidBodyTypeComponent(RigidBodyType::Dynamic),
-            velocity: RigidBodyVelocityComponent(
-                RigidBodyVelocity::new(Vector::new(1.0, 1.0), Default::default())
-            ),
-            mass_properties: Default::default(),
-            forces: Default::default(),
-            activation: Default::default(),
-            damping: Default::default(),
-            dominance: Default::default(),
-            ccd: Default::default(),
-            changes: Default::default(),
-            ids: Default::default(),
-            colliders: Default::default(),
-            ..Default::default()
-        })
         .insert_bundle(SpriteBundle {
             sprite: Sprite {
                 color: Color::rgb_u8(255, 50, 50),
                 custom_size: Some(Vec2::new(10.0, 10.0)),
                 ..Default::default()
             },
-            transform: Transform::from_xyz(-495.0, 500.0, 0.0),
+            // transform: Transform::from_xyz(-500.0, 500.0, 0.0),
             ..Default::default()
-        });
+        })
+    ;
 }
 
 #[derive(Component)]
 struct Brick;
 
+#[derive(Component)]
+struct Ball;
